5c2b7989a925dfe8cf0b9dacab26e12e20dd8afc
鸭鸭 <86056701+mmyo456@users.noreply.github.com>
2024-12-31 18:03:37 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,10 +5,8 @@
         <span>
           <span :class="startYear < fullYear ? 'c-hidden' : 'hidden'">Copyright&nbsp;</span>
           &copy;
-          <span v-if="startYear < fullYear"
-            class="site-start">
-            {{ startYear }}
-            -
+          <span v-if="startYear < fullYear" class="site-start">
+            {{ startYear }} -
           </span>
           {{ fullYear }}
           <a :href="siteUrl">{{ siteAuthor }}</a>
@@ -25,6 +23,14 @@
           &amp;
           <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
             {{ siteIcp }}
+          </a>
+        </span>
+        <!-- 公安备案 -->
+        <span>
+          &amp;
+          <img src="/beian.png" class="net" />
+          <a v-if="siteIcpa" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33030202002197" target="_blank">
+            {{ siteIcpa }}
           </a>
         </span>
       </div>
@@ -46,20 +52,21 @@ import { MusicOne } from "@icon-park/vue-next";
 import { mainStore } from "@/store";
 import config from "@/../package.json";
 
+// 获取当前年份
 const store = mainStore();
 const fullYear = new Date().getFullYear();
 
-// 加载配置数据
-// const siteStartDate = ref(import.meta.env.VITE_SITE_START);
+// 加载站点信息
 const startYear = ref(
   import.meta.env.VITE_SITE_START?.length >= 4 ? 
   import.meta.env.VITE_SITE_START.substring(0, 4) : null
 );
-const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
-const siteAuthor = ref(import.meta.env.VITE_SITE_AUTHOR);
+const siteIcp = ref(import.meta.env.VITE_SITE_ICP);  // 获取站点备案号
+const siteIcpa = ref(import.meta.env.VITE_SITE_ICPA);  // 获取公安备案号
+const siteAuthor = ref(import.meta.env.VITE_SITE_ANTHOR); // 获取站点作者
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "https://www.imsyy.top";
+  if (!url) return "https://www.imsyy.top"; // 默认网址
   // 判断协议前缀
   if (!url.startsWith("http://") && !url.startsWith("https://")) {
     return "//" + url;
@@ -79,27 +86,40 @@ const siteUrl = computed(() => {
   text-align: center;
   z-index: 0;
   font-size: 14px;
-  // 文字不换行
   word-break: keep-all;
   white-space: nowrap;
+
   .power {
     animation: fade 0.3s;
+    span {
+      display: inline-flex;
+      align-items: center;
+      gap: 4px; /* 增加间距 */
+    }
+
+    a {
+      margin: 0 2px; /* 链接与文本的间距 */
+    }
   }
+
   .lrc {
     padding: 0 20px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
+
     .lrc-all {
       width: 98%;
       display: flex;
       flex-direction: row;
       justify-content: center;
       align-items: center;
+
       .lrc-text {
         margin: 0 8px;
       }
+
       .i-icon {
         width: 18px;
         height: 18px;
@@ -107,26 +127,31 @@ const siteUrl = computed(() => {
       }
     }
   }
+
   &.blur {
     backdrop-filter: blur(10px);
     background: rgb(0 0 0 / 25%);
     font-size: 16px;
   }
+
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 0.15s ease-in-out;
   }
+
   @media (max-width: 720px) {
     font-size: 0.9rem;
     &.blur {
       font-size: 0.9rem;
     }
   }
+
   @media (max-width: 560px) {
     .c-hidden {
       display: none;
     }
   }
+
   @media (max-width: 480px) {
     .hidden {
       display: none;
