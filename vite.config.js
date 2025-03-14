1f921f777204585df359b3cf30733778e5233d9c
鸭鸭 <86056701+mmyo456@users.noreply.github.com>
2025-03-14 21:52:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -106,8 +106,9 @@ export default ({ mode }) =>
     css: {
       preprocessorOptions: {
         scss: {
-          charset: false,
-          additionalData: `@import "./src/style/global.scss";`,
+          api: 'modern',
+           additionalData: `@use "./src/style/global.scss" as *;`,
+           silenceDeprecations: ["legacy-js-api"],
         },
       },
     },
